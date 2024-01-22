f3aadc79e14f478e18cbee90cca1ffd996a90e0b
UgaGithub1 <156825786+UgaGithub1@users.noreply.github.com>
2024-01-22 22:48:15 +0500

    Add files via upload

---
+++ B2.1.py
@@ -0,0 +1,12 @@
+some_string = "value"
+
+
+vnfjivndfjn = "other_value"
+
+a, b = "some_string_a", "some_string_b"
+temp = a # сохранили в temp старое значение a
+a = b # в переменную a сохранили значение b
+b = temp # а в переменную b вернули сохраненное значение a
+a, b = b, a
+text = input("Программа переменных")
+name = input("Лекс Лютер")
